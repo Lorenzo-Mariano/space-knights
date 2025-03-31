b8f8c319720114bc5e9285c383556d05ae64577d
Lorenzo Mariano <hhumminah@gmail.com>
2025-03-31 09:37:08 +0800

    Add cutscenes, no prologue, no epilogue

--- Assets/Script/NextLevel.cs
+++ Assets/Script/NextLevel.cs
@@ -14,7 +14,7 @@ public class NextLevel : MonoBehaviour
         
         // Could use other.GetComponent<Player>() to see if the game object has a Player component
         // Tags work too. Maybe some players have different script components?
-        if(other.tag == "Player") {
+        if (other.tag == "Player") {
             // Player entered, so move level
             print("Switching Scene to " + sceneBuildIndex);
             SceneManager.LoadScene(sceneBuildIndex, LoadSceneMode.Single);
